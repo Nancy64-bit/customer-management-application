d37d844fd0776fd39290fab2237e70b577cd1511
HackerGwen <MissZeaffy@outlook.com>
2023-08-04 20:26:24 +0530

    Project Initialized

---
+++ src/main/java/com/sunbasedata/customermanagement/customermanagementapp/CustomerManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.sunbasedata.customermanagement.customermanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementAppApplication.class, args);
+	}
+
+}
